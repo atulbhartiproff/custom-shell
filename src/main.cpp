0d251650d7a705148bcec8a8fced8aab695bc76f
Atul <atulbhartiproff@gmail.com>
2026-05-22 18:02:37 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sstream>
 #include <string>
 
 int main() {
@@ -6,22 +7,36 @@ int main() {
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
+  std::string line;
+  std::string command;
+
   while (true) {
     std::cout << "$ ";
-    std::string command;
-    std::getline(std::cin, command);
-    if(command=="exit") break;
-    else if(command.substr(0,5)=="echo ")
-    {
-        std::cout<<command.substr(5,command.size())<<std::endl;
+
+    std::getline(std::cin, line);
+    std::stringstream ss(line);
+    ss>>command;
+
+    if (command == "exit")
+      break;
+
+    else if (command == "echo") {
+      std::string word;
+      while(ss>>word)
+      {
+          std::cout<<word<<" ";
+      }
+      std::cout<<std::endl;
     }
-    else if(command.substr(0,5)=="type ")
-    {
-        std::string inp=command.substr(5,command.size());
-        if(inp =="echo" || inp=="exit" || inp=="type") std::cout<<inp<<" is a shell builtin"<<std::endl;
-        else std::cout<<inp<<": not found"<<std::endl;
-    }
-    else
-    std::cout << command << ": command not found" << std::endl;
+
+    else if (command == "type") {
+        std::string potentialcommand;
+        ss>>potentialcommand;
+      if (potentialcommand == "echo" || potentialcommand == "exit" || potentialcommand == "type")
+          std::cout << potentialcommand << " is a shell builtin" << std::endl;
+      else
+        std::cout << potentialcommand << ": not found" << std::endl;
+    } else
+      std::cout << command << ": command not found" << std::endl;
   }
 }
