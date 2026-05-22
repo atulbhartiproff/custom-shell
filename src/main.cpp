bdc58681bd7bf765c8e572c3799cbe84656fabfd
Atul <atulbhartiproff@gmail.com>
2026-05-22 17:16:00 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,10 @@ int main() {
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
+  std::string command;
+  std::getline(std::cin,command);
+
+  std::cout<<command<<": command not found";
   // TODO: Uncomment the code below to pass the first stage
   std::cout << "$ ";
 }
